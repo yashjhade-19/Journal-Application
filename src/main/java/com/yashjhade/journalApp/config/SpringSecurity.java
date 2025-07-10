0d0859a56ccdb62e9cd8abd3b6cb71bca6a5321f
yyyyjjj <yashjhade10@gmail.com>
2025-07-11 01:11:37 +0530

    .

--- src/main/java/com/yashjhade/journalApp/config/SpringSecurity.java
+++ src/main/java/com/yashjhade/journalApp/config/SpringSecurity.java
@@ -15,6 +15,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -24,7 +29,7 @@ public class SpringSecurity {
     private JwtFilter jwtFilter;
 
     @Autowired
-    private  UserDetailsService userDetailsService;
+    private UserDetailsService userDetailsService;
 
     public SpringSecurity(UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
@@ -38,7 +43,7 @@ public class SpringSecurity {
                 .antMatchers("/public/**").permitAll()
 
                 .antMatchers("/user/**", "/journal/**").authenticated()
-                .antMatchers( "/admin/**").hasRole("ADMIN")
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .and()
 
                 .sessionManagement()
@@ -48,8 +53,7 @@ public class SpringSecurity {
     }
 
     @Bean
-    public
-    DaoAuthenticationProvider authProvider() {
+    public DaoAuthenticationProvider authProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder());
@@ -65,5 +69,21 @@ public class SpringSecurity {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList(
+                "http://localhost:3000",  // Your React app
+                "https://your-production-domain.com"
+        ));
+        config.setAllowedMethods(Arrays.asList("*")); // GET, POST, etc.
+        config.setAllowedHeaders(Arrays.asList("*")); // Allow JWT header
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config); // Apply to all routes
+        return source;
     }
 }
