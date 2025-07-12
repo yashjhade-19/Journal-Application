47b9a90112571aedbf7fb7ff52e3eb1c147ba858
yyyyjjj <yashjhade10@gmail.com>
2025-07-12 19:31:25 +0530

    ready for frontend

---
+++ src/main/java/com/yashjhade/journalApp/dto/JournalEntryDTO.java
@@ -0,0 +1,28 @@
+package com.yashjhade.journalApp.dto;
+
+import com.yashjhade.journalApp.enums.Sentiment;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import javax.validation.constraints.NotEmpty;
+
+import java.util.Date;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JournalEntryDTO {
+
+
+    @NotEmpty
+    @Schema(description = "The user's JE title")
+    private String title;
+    private String content;
+    private Date date;
+    @NotEmpty
+    private Sentiment sentiment;
+}
---
+++ src/main/java/com/yashjhade/journalApp/dto/UserDTO.java
@@ -0,0 +1,23 @@
+
+package com.yashjhade.journalApp.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    @NotEmpty
+    @Schema(description = "The user's username")
+    private String userName;
+    private String email;
+    private boolean sentimentAnalysis;
+    @NotEmpty
+    private String password;
+}
