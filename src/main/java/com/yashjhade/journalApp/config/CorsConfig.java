b0132f252bb6ad23c4748ddc568368362cc24cfa
yyyyjjj <yashjhade10@gmail.com>
2025-07-15 02:04:47 +0530

    Resolved CORS for live deployment

--- src/main/java/com/yashjhade/journalApp/config/CorsConfig.java
+++ src/main/java/com/yashjhade/journalApp/config/CorsConfig.java
@@ -1,20 +1,20 @@
 package com.yashjhade.journalApp.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Your frontend URL
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://journal-application-production.up.railway.app"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
-
